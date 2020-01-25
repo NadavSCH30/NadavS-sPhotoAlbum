971f22598c4095d9ed6a64b9bc380e50aab0fe77
Nadav Schneider <nadav.schneider10@gmail.com>
2020-01-25 20:29:58 +0200

    added imports in views,py

--- NadavS'sPhotoAlbum/NadavS_sPhotoAlbum/views.py
+++ NadavS'sPhotoAlbum/NadavS_sPhotoAlbum/views.py
@@ -1,13 +1,38 @@
 """
 Routes and views for the flask application.
 """
-
 from datetime import datetime
 from flask import render_template
 from NadavS_sPhotoAlbum import app
-# from flask_wtf import FlaskForm
-# from wtfform import StringField, SubmitField
-# from wtform.validators import DataRequired 
+
+
+from datetime import datetime
+from flask import render_template, redirect, request
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import json 
+import requests
+
+import io
+import base64
+
+from os import path
+
+from flask   import Flask, render_template, flash, request
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+from wtforms import TextField, TextAreaField, SubmitField, SelectField, DateField
+from wtforms import ValidationError
+
+
+from NadavS_sPhotoAlbum.Models.module1 import QueryFormStructure 
+
 
 app.config['SECRET_KEY'] = 'The first argument to the field'
 
@@ -53,12 +78,30 @@ def Photos():
     )
 @app.route('/Query')
 def Query():
-    """Renders the photos page."""
-    return render_template(
-        'Query.html',
-        title='Query',
-        year=datetime.now().year,
-        message='This is my photo album'
+    Name = None
+    capital = ''
+    df = pd.read_csv(path.join(path.dirname(__file__), 'static\\Data\\capitals.csv'))
+    df = df.set_index('Country')
+
+    form = QueryFormStructure(request.form)
+     
+    if (request.method == 'POST' ):
+        name = form.name.data
+        if (name in df.index):
+            capital = df.loc[name,'Capital']
+        else:
+            capital = name + ', no such country'
+        form.name.data = ''
+
+    raw_data_table = df.to_html(classes = 'table table-hover')
+
+    return render_template('Query.html', 
+            form = form, 
+            name = capital, 
+            raw_data_table = raw_data_table,
+            title='Query by the user',
+            year=datetime.now().year,
+            message='This page will use the web forms to get user input'
         )
 
 # class ShootingsForm(FlaskForm):
